a88ad934b497ebe35eddccd3633f16dc311f6e85
Borna Bosnjak <79083667+EX3mKing@users.noreply.github.com>
2025-03-04 20:42:13 +0100

    fin ig

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class BoardManager : NetworkBehaviour
 {
     private Button[,] _buttons = new Button[3, 3];
+    private int[,] _states = new int[3, 3];
     
     public override void OnNetworkSpawn()
     {
@@ -36,17 +37,23 @@ public class BoardManager : NetworkBehaviour
         if (NetworkManager.Singleton.IsHost && GameManager.Instance.isHostsTurn.Value == 0)
         {
             _buttons[r, c].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "X";
+            _buttons[r, c].interactable = false;
+            _states[r, c] = 1;
+            Debug.Log(CheckWinState(1));
             ChangeSpriteClientRpc(r, c);
             GameManager.Instance.isHostsTurn.Value = 1;
-            _buttons[r, c].interactable = false;
+
+            if (CheckWinState(1)) GameManager.Instance.gameState.Value = 1;
+            if (CheckDrawState()) GameManager.Instance.gameState.Value = 3;
+            
         }
         else if (!NetworkManager.Singleton.IsHost && GameManager.Instance.isHostsTurn.Value == 1)
         {
             _buttons[r, c].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "O";
-            ChangeSpriteServerRpc(r, c);
             _buttons[r, c].interactable = false;
+            _states[r, c] = 2;
+            ChangeSpriteServerRpc(r, c);
         }
-
     }
 
     [ClientRpc]
@@ -54,6 +61,7 @@ public class BoardManager : NetworkBehaviour
     {
         _buttons[r, c].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "X";
         _buttons[r, c].interactable = false;
+        _states[r, c] = 1;
     }
 
     [ServerRpc(RequireOwnership = false)]
@@ -61,6 +69,39 @@ public class BoardManager : NetworkBehaviour
     {
         _buttons[r, c].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "O";
         _buttons[r, c].interactable = false;
+        _states[r, c] = 2;
+        
         GameManager.Instance.isHostsTurn.Value = 0;
+        
+        if (CheckWinState(2)) GameManager.Instance.gameState.Value = 2;
+        if (CheckDrawState()) GameManager.Instance.gameState.Value = 3;
+        
+    }
+    
+    private bool CheckWinState(int sender)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            if (_states[i, 0] == _states[i, 1] && _states[i, 1] == _states[i, 2] && _states[i, 2] == sender) return true;
+            if (_states[0, i] == _states[1, i] && _states[1,i] == _states[2, i] && _states[2, i] == sender) return true;
+        }
+
+        if (_states[1, 1] == sender)
+        {
+            if (_states[0, 0] == _states[1, 1] && _states[1, 1] == _states[2, 2]) return true;
+            if (_states[2, 0] == _states[1, 1] && _states[1, 1] == _states[0, 2]) return true;
+        }
+        
+        return false;
+    }
+
+    private bool CheckDrawState()
+    {
+        foreach (var state in _states)
+        {
+            if (state == 0) return false;
+        }
+
+        return true;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,7 +8,20 @@ public class GameManager : NetworkBehaviour
     public static GameManager Instance;
     [SerializeField] private GameObject p_board;
 
+    // 0    -   is hosts turn
+    // 1    -   clients turn
+    // else -   neutral / locked
     public NetworkVariable<int> isHostsTurn = new NetworkVariable<int>(0);
+    
+    // 0 - neutral
+    // 1 - host win
+    // 2 - client win
+    // 3 - draw
+    public NetworkVariable<int> gameState = new NetworkVariable<int>(0); 
+
+    [SerializeField] private GameObject o_resultPanel;
+    [SerializeField] private GameObject o_joinUI;
+    [SerializeField] private TextMeshProUGUI tmp_resultText;
     
     private void Awake()
     {
@@ -30,8 +44,40 @@ public class GameManager : NetworkBehaviour
             {
                 Debug.Log("Spawn shi");
                 Instantiate(p_board).GetComponent<NetworkObject>().Spawn();
+                o_joinUI.SetActive(false);
+                HideRelayUIClientRPC();
             }
         };
+
+        gameState.OnValueChanged += (oldValue, newValue) =>
+        {
+            if (newValue != 0) o_resultPanel.SetActive(true);
+            switch (newValue)
+            {
+                case 0:
+                    break;
+                case 1:
+                    tmp_resultText.text = "Host won";
+                    break;
+                case 2:
+                    tmp_resultText.text = "Client won";
+                    break;
+                case 3:
+                    tmp_resultText.text = "Draw";
+                    break;
+                default:
+                    Debug.LogError("Something went wrong");
+                    break;
+            }
+            
+            Debug.Log("shi changed");
+        };
+    }
+
+    [ClientRpc]
+    private void HideRelayUIClientRPC()
+    {
+        o_joinUI.SetActive(false);
     }
 
     public void StartHost()
@@ -43,5 +89,38 @@ public class GameManager : NetworkBehaviour
     {
         NetworkManager.Singleton.StartClient();
     }
-    
+
+    public void RestartGame()
+    {
+        if (IsHost)
+        {
+            FindAnyObjectByType<BoardManager>().GetComponent<NetworkObject>().Despawn();
+            gameState.Value = 0;
+            isHostsTurn.Value = 0;
+            RestartGameClientRpc();
+            o_resultPanel.SetActive(false);
+            Instantiate(p_board).GetComponent<NetworkObject>().Spawn();
+        }
+        else
+        {
+            RestartGameServerRpc();
+        }
+        
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RestartGameServerRpc()
+    {
+        FindAnyObjectByType<BoardManager>().GetComponent<NetworkObject>().Despawn();
+        o_resultPanel.SetActive(false);
+        Instantiate(p_board).GetComponent<NetworkObject>().Spawn();
+        gameState.Value = 0;
+        isHostsTurn.Value = 0;
+    }
+
+    [ClientRpc]
+    private void RestartGameClientRpc()
+    {
+        o_resultPanel.SetActive(false);
+    }
 }
